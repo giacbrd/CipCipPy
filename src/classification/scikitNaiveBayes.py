5bf6eac445aa9fd69199271413fcc28085367e4f
enfry <diego.marcheggiani@isti.cnr.it>
2013-12-10 11:59:44 +0100

    Added idf vectorization insetad of tf

--- src/classification/scikitNaiveBayes.py
+++ src/classification/scikitNaiveBayes.py
@@ -1,6 +1,6 @@
 """Classification with scikit-learn"""
 
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import svm
 
@@ -18,15 +18,17 @@ class TrainingSet():
             self.tweetTarget.append(1 if triple[1] else 0)
             self.features.append(' '.join(triple[2]))
         self.vectorcounts = None
-        self.count_vect = CountVectorizer(min_df=1)
+        self.count_vect = CountVectorizer(min_df=1, binary=True)
+        self.idf_transf = TfidfTransformer()
 
     def countVectorize(self):
-        """Compute vectors of features frequency"""
+        """Compute vectors of features presence (binary count), and inverse document frequency"""
         self.vectorcounts = self.count_vect.fit_transform(self.features)
+        self.vectoridf = self.idf_transf.fit_transform(self.vectorcounts)
 
     def vectorizeTest(self, testTweet):
-        """Vectorize a tweet"""
-        return self.count_vect.transform([' '.join(testTweet[2])])
+        """Vectorize a tweet with idf"""
+        return self.idf_transf.transform(self.count_vect.transform([' '.join(testTweet[2])]))
 
     def addExample(self, rawTweet):
         """Add a new example for retraining"""
